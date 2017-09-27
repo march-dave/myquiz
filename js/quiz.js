2f70208534283e2d62e0101b5acf727c2c78b736
march-dave <ysangy@gmail.com>
2017-09-26 20:38:24 -0400

    code fight

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,19 @@
-(function(){
+(function() {
 
-	// script goes here
+    // script goes here
+    function adjacentElementsProduct(inputArray) {
+        var arr = [];
+        for (var i = 1; i < inputArray.length; i++) {
+            arr.push(inputArray[i] * inputArray[i - 1]);
+        }
+
+        function compareNum(a, b) {
+            return a - b;
+        }
+        arr.sort(compareNum).reverse()
+        return arr[0]
+    }
+
+    adjacentElementsProduct([5, 6, -4, 2, 3, 2, -23])
 
 })();
