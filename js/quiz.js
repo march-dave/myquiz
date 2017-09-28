caa7ec904083809e809e367932d658b8b408ec0a
march-dave <ysangy@gmail.com>
2017-09-27 20:32:08 -0400

    up to date

--- js/quiz.js
+++ js/quiz.js
@@ -1,19 +1,21 @@
 (function() {
 
-    // script goes here
-    function adjacentElementsProduct(inputArray) {
-        var arr = [];
-        for (var i = 1; i < inputArray.length; i++) {
-            arr.push(inputArray[i] * inputArray[i - 1]);
-        }
+    // // script goes here
+    // function adjacentElementsProduct(inputArray) {
+    //     var arr = [];
+    //     for (var i = 1; i < inputArray.length; i++) {
+    //         arr.push(inputArray[i] * inputArray[i - 1]);
+    //     }
 
-        function compareNum(a, b) {
-            return a - b;
-        }
-        arr.sort(compareNum).reverse()
-        return arr[0]
-    }
+    //     function compareNum(a, b) {
+    //         return a - b;
+    //     }
+    //     arr.sort(compareNum).reverse()
+    //     return arr[0]
+    // }
 
-    adjacentElementsProduct([5, 6, -4, 2, 3, 2, -23])
+    // adjacentElementsProduct([5, 6, -4, 2, 3, 2, -23])
+
+
 
 })();
