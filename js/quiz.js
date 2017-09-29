f6a294124fbba105b93e1fd96fb5aa717afd9714
march-dave <ysangy@gmail.com>
2017-09-29 04:28:24 -0400

    javascript practice Array DS

--- js/quiz.js
+++ js/quiz.js
@@ -54,3 +54,17 @@
 // Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
 
 // A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+
+// function main() {
+//     var n = parseInt(readLine());
+//     arr = readLine().split(' ');
+
+//     var push = [];
+//     arr = arr.map( Number )
+
+//     //var s = arr.sort( (a, b) => { return a + b } ).join(' ');
+
+//     var arr2 = arr.sort( (a, b) => { return a + b } );
+
+//     console.log( arr2 );
+// }
